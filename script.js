84234c673b603ab59357449f1a39636e7e6738c5
Dikshij <dikshi.01@gmail.com>
2022-01-25 17:25:46 +0530

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('hello!')
+function clickedMe() {
+  console.log('hello!');
+  document.getElementById('dikshi1').innerHTML = '';
+  var card = document.getElementById('dikshi1').innerHTML;
+
+  card = document.createElement('div');
+  card.className = 'card shadow cursor-pointer';
+
+  let cardBody = document.createElement('div');
+  cardBody.className = 'card-body';
+
+  let title = document.createElement('h5');
+  title.innerText = 'ff';
+  title.className = 'card-title';
+
+  let color = document.createElement('div');
+  color.innerText = 'try';
+  color.className = 'black';
+
+  cardBody.appendChild(title);
+  cardBody.appendChild(color);
+  card.appendChild(cardBody);
+  dikshi1.appendChild(card);
+  document.getElementById('img1').src =
+    'https://bootstrapmade.com/demo/templates/Maxim/assets/img/about-img.jpg';
+}
