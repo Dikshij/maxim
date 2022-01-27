77f9388f4ff99121f08fc3464e32a113ef5a9ad2
Dikshij <dikshi.01@gmail.com>
2022-01-27 18:19:07 +0530

    3 files modified, 1 added

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 function clickedMe() {
-  console.log('hello!');
-  document.getElementById('dikshi1').innerHTML = '';
-  var card = document.getElementById('dikshi1').innerHTML;
+  document.getElementById('dikshi1');
+  var card = (document.getElementById('dikshi1').innerHTML = '');
 
   card = document.createElement('div');
-  card.className = 'card shadow cursor-pointer';
+
+  card.className = 'card  shadow cursor-pointer';
 
   let cardBody = document.createElement('div');
   cardBody.className = 'card-body';
 
   let title = document.createElement('h5');
-  title.innerText = 'ff';
+  title.innerText = 'Modi is est';
   title.className = 'card-title';
 
   let color = document.createElement('div');
-  color.innerText = 'try';
+  color.innerText =
+    'Who will catch him,but the whole of them are none where you will encounter him';
   color.className = 'black';
 
   cardBody.appendChild(title);
@@ -23,4 +24,208 @@ function clickedMe() {
   dikshi1.appendChild(card);
   document.getElementById('img1').src =
     'https://bootstrapmade.com/demo/templates/Maxim/assets/img/features-1.png';
+
+  //Make card2 original card
+
+  document.getElementById('dikshi2');
+  var card1 = (document.getElementById('dikshi2').innerHTML = '');
+
+  card1 = document.createElement('div');
+
+  card1.className = 'card border-0';
+
+  let cardBody1 = document.createElement('div');
+  cardBody1.className = 'card-body';
+
+  let title1 = document.createElement('h5');
+  title1.innerText = 'The Source Of The Present';
+  title1.className = 'card-title';
+
+  let color1 = document.createElement('div');
+  color1.innerText = 'Pleaseure or repedition by which to catch him';
+  color1.className = 'black';
+
+  cardBody1.appendChild(title1);
+  cardBody1.appendChild(color1);
+  card1.appendChild(cardBody1);
+  dikshi2.appendChild(card1);
+
+  //Make card 3 normal
+
+  document.getElementById('dikshi3');
+  var card3 = (document.getElementById('dikshi3').innerHTML = '');
+
+  card3 = document.createElement('div');
+
+  card3.className = 'card  border-0';
+
+  let cardBody3 = document.createElement('div');
+  cardBody3.className = 'card-body';
+
+  let title3 = document.createElement('h5');
+  title3.innerText = 'I will explain or';
+  title3.className = 'card-title';
+
+  let color3 = document.createElement('div');
+  color3.innerText = 'She wants to be pardoned,no fuzzy flattery is born';
+  color3.className = 'black';
+
+  cardBody3.appendChild(title3);
+  cardBody3.appendChild(color3);
+  card3.appendChild(cardBody3);
+  dikshi3.appendChild(card3);
+}
+
+function clickedMe2() {
+  document.getElementById('dikshi2');
+  var card = (document.getElementById('dikshi2').innerHTML = '');
+
+  card = document.createElement('div');
+
+  card.className = 'card  shadow cursor-pointer';
+
+  let cardBody = document.createElement('div');
+  cardBody.className = 'card-body';
+
+  let title = document.createElement('h5');
+  title.innerText = 'The Source Of The Present';
+  title.className = 'card-title';
+
+  let color = document.createElement('div');
+  color.innerText = 'Pleaseure or repedition by which to catch him';
+  color.className = 'black';
+
+  cardBody.appendChild(title);
+  cardBody.appendChild(color);
+  card.appendChild(cardBody);
+  dikshi2.appendChild(card);
+  document.getElementById('img1').src =
+    'https://bootstrapmade.com/demo/templates/Maxim/assets/img/features-2.png';
+
+  //Make card 1 as original card
+
+  document.getElementById('dikshi1');
+  var card2 = (document.getElementById('dikshi1').innerHTML = '');
+  card2 = document.createElement('div');
+
+  card2.className = 'card border-0';
+
+  let cardBody1 = document.createElement('div');
+  cardBody1.className = 'card-body';
+
+  let title1 = document.createElement('h5');
+  title1.innerText = 'Modi is est';
+  title1.className = 'card-title';
+
+  let color1 = document.createElement('div');
+  color1.innerText =
+    'Who will catch him,but the whole of them are none where you will encounter him';
+  color1.className = 'black';
+
+  cardBody1.appendChild(title1);
+  cardBody1.appendChild(color1);
+  card2.appendChild(cardBody1);
+  dikshi1.appendChild(card2);
+
+  //Make card 3 normal
+
+  document.getElementById('dikshi3');
+  var card3 = (document.getElementById('dikshi3').innerHTML = '');
+
+  card3 = document.createElement('div');
+
+  card3.className = 'card  border-0';
+
+  let cardBody3 = document.createElement('div');
+  cardBody3.className = 'card-body';
+
+  let title3 = document.createElement('h5');
+  title3.innerText = 'I will explain or';
+  title3.className = 'card-title';
+
+  let color3 = document.createElement('div');
+  color3.innerText = 'She wants to be pardoned,no fuzzy flattery is born';
+  color3.className = 'black';
+
+  cardBody3.appendChild(title3);
+  cardBody3.appendChild(color3);
+  card3.appendChild(cardBody3);
+  dikshi3.appendChild(card3);
+}
+
+function clickedMe3() {
+  document.getElementById('dikshi3');
+  var card = (document.getElementById('dikshi3').innerHTML = '');
+
+  card = document.createElement('div');
+
+  card.className = 'card  shadow cursor-pointer';
+
+  let cardBody = document.createElement('div');
+  cardBody.className = 'card-body';
+
+  let title = document.createElement('h5');
+  title.innerText = 'I will explain or';
+  title.className = 'card-title';
+
+  let color = document.createElement('div');
+  color.innerText = 'She wants to be pardoned,no fuzzy flattery is born';
+  color.className = 'black';
+
+  cardBody.appendChild(title);
+  cardBody.appendChild(color);
+  card.appendChild(cardBody);
+  dikshi3.appendChild(card);
+  document.getElementById('img1').src =
+    'https://bootstrapmade.com/demo/templates/Maxim/assets/img/features-3.png';
+
+  //Make card 1 original card
+
+  document.getElementById('dikshi1');
+  var card2 = (document.getElementById('dikshi1').innerHTML = '');
+  card2 = document.createElement('div');
+
+  card2.className = 'card border-0';
+
+  let cardBody2 = document.createElement('div');
+  cardBody2.className = 'card-body';
+
+  let title2 = document.createElement('h5');
+  title2.innerText = 'Modi is est';
+  title2.className = 'card-title';
+
+  let color2 = document.createElement('div');
+  color2.innerText =
+    'Who will catch him,but the whole of them are none where you will encounter him';
+  color2.className = 'black';
+
+  cardBody2.appendChild(title2);
+  cardBody2.appendChild(color2);
+  card2.appendChild(cardBody2);
+  dikshi1.appendChild(card2);
+
+  //Make card2 original card
+
+  document.getElementById('dikshi2');
+  var card1 = (document.getElementById('dikshi2').innerHTML = '');
+
+  card1 = document.createElement('div');
+
+  card1.className = 'card border-0';
+
+  let cardBody1 = document.createElement('div');
+  cardBody1.className = 'card-body';
+
+  let title1 = document.createElement('h5');
+  title1.innerText = 'The Source Of The Present';
+  title1.className = 'card-title';
+
+  let color1 = document.createElement('div');
+  color1.innerText = 'Pleaseure or repedition by which to catch him';
+  color1.className = 'black';
+
+  cardBody1.appendChild(title1);
+  cardBody1.appendChild(color1);
+  card1.appendChild(cardBody1);
+  dikshi2.appendChild(card1);
 }
